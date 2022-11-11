14635a04d98f7c8f16fa10b9a3f6db399afdede7
witwint <witwint1234@gmail.com>
2022-11-11 22:19:21 +0900

    save

--- src/main/java/com/community/numble/app/board/controller/MemberApiController.java
+++ src/main/java/com/community/numble/app/board/controller/MemberApiController.java
@@ -1,9 +1,14 @@
 package com.community.numble.app.board.controller;
 
+import com.community.numble.app.board.domain.Member;
+import com.community.numble.app.board.dto.MemberDto.CreateMemberRequest;
+import com.community.numble.app.board.dto.MemberDto.CreateMemberResponse;
 import com.community.numble.app.board.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +19,20 @@ public class MemberApiController {
 	private final MemberService memberService;
 
 	@PostMapping("/numble11/members")
-	public ResponseEntity saveMember()
+	public ResponseEntity saveMember(@RequestBody CreateMemberRequest request) {
+		Member member = new Member();
+		member.setName(request.getName());
+
+		Long id = memberService.join(member);
+		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new CreateMemberResponse(id));
+	}
+
+	@PostMapping("/numble11/members2")
+	public CreateMemberResponse saveMemberv2(@RequestBody CreateMemberRequest request) {
+		Member member = new Member();
+		member.setName(request.getName());
+
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
+	}
 }
