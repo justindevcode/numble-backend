4e077658f43f3e84ebd5a96d589d073a4dc93ca0
jeon-eunseong <dmstjd1024@naver.com>
2022-11-13 15:08:15 +0900

    feat: spring docs 사용한 swagger 적용

--- src/main/java/com/community/numble/app/user/controller/UserController.java
+++ src/main/java/com/community/numble/app/user/controller/UserController.java
@@ -3,14 +3,17 @@ package com.community.numble.app.user.controller;
 import com.community.numble.app.user.dto.UserCreateDto;
 import com.community.numble.app.user.service.UserService;
 import com.community.numble.common.service.ResponseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "User", description = "사용자 관련 api")
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/v1/user")
 public class UserController {
     @Autowired
     private UserService userService;
@@ -18,7 +21,7 @@ public class UserController {
     @Autowired
     private ResponseService responseService;
 
-
+    @Operation(summary = "회원가입 메서드", description = "회원가입 메서드입니다.")
     @PostMapping("/join")
     public ResponseEntity<?> register(UserCreateDto userCreateDto){
 
