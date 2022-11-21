a2d5002b8549731066dfde98b9c4787df5b6159d
witwint <witwint1234@gmail.com>
2022-11-21 15:41:22 +0900

    swagger test

--- src/main/java/com/community/numble/app/board/controller/MemberApiController.java
+++ src/main/java/com/community/numble/app/board/controller/MemberApiController.java
@@ -4,6 +4,8 @@ import com.community.numble.app.board.domain.Member;
 import com.community.numble.app.board.dto.MemberDto.CreateMemberRequest;
 import com.community.numble.app.board.dto.MemberDto.CreateMemberResponse;
 import com.community.numble.app.board.service.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@Tag(name = "MemberTest", description = "사용자 관련 api")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1")
 public class MemberApiController {
@@ -22,6 +25,7 @@ public class MemberApiController {
 	private final MemberService memberService;
 
 	@PostMapping("/numble11/members")
+	@Operation(summary = "회원가입 테스트 메서드", description = "회원가입 테스트 메서드입니다.")
 	public ResponseEntity saveMember(@RequestBody CreateMemberRequest request) {
 		Member member = new Member();
 		member.setName(request.getName());
