ffd55648562964b8d33ac582ea6e1b5fc26b0772
witwint <witwint1234@gmail.com>
2022-11-23 21:55:10 +0900

    수정

--- src/main/java/com/community/numble/app/user/dto/UserCreateDto.java
+++ src/main/java/com/community/numble/app/user/dto/UserCreateDto.java
@@ -5,7 +5,6 @@ import com.community.numble.common.utils.DateUtils;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
